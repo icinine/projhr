8a8fddef08ef4cbe5fa9ca1fc99bc0f015cdcdcb
icinine <shane.carr@sitesignals.com>
2015-04-14 14:33:34 +0000

    dept dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :depts
+
   resources :profiles
 
   resources :employees
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326221311) do
+ActiveRecord::Schema.define(version: 20150414135052) do
+
+  create_table "depts", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string   "name"
