e1e2319f7a06a53d809eecc412f017c635611704
icinine <shane.carr@sitesignals.com>
2015-04-15 13:26:37 +0000

    update

--- app/models/vacation_search.rb
+++ app/models/vacation_search.rb
@@ -3,7 +3,7 @@ class VacationSearch
   
   def initialize(params)
     params ||= {}
-    @date_from = parsed_date(params[:date_from], 30.days.ago.to_date.to_s)
+    @date_from = parsed_date(params[:date_from], 300.days.ago.to_date.to_s)
     @date_to = parsed_date(params[:date_to], Date.today.to_s)
 
   end
